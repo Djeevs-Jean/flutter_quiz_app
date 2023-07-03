8932bc9ce8df5cdf81d4b4ed29f13e052b609146
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-07-02 23:32:41 -0400

    delete quizResult

--- lib/screens/activity_page.dart
+++ lib/screens/activity_page.dart
@@ -11,6 +11,190 @@ class ActivityTab extends StatefulWidget {
   @override
   State<ActivityTab> createState() => _ActivityTabState();
 }
+
+class _ActivityTabState extends State<ActivityTab> {
+  late Future<List<QuizResult>> _quizResultsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _quizResultsFuture = fetchQuizResults();
+  }
+
+  Future<List<QuizResult>> fetchQuizResults() async {
+    final quizResultData = QuizResultStorage();
+    List<QuizResult> quizResults = await quizResultData.getQuizResults();
+    return quizResults.reversed.toList();
+  }
+
+  void deleteQuizResult(QuizResult quizResult) async {
+    print('Deleting quiz result with ID: ${quizResult.id}');
+    final quizResultData = QuizResultStorage();
+    await quizResultData.deleteQuizResult(quizResult);
+
+    setState(() {
+      _quizResultsFuture = fetchQuizResults();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<QuizResult>>(
+      future: _quizResultsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.purple,
+              strokeWidth: 6,
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              'Error: ${snapshot.error}',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+        } else if (snapshot.hasData) {
+          final quizResults = snapshot.data!;
+          if (quizResults.isEmpty) {
+            return Center(
+              child: Text(
+                'No quiz results found.',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          } else {
+            return ListView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+            itemCount: quizResults.length,
+            itemBuilder: (context, index) {
+              final quizResult = quizResults[index];
+              return Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ListTile(
+                  title: Text(
+                    'activityname'.tr() + ' : ${quizResult.quizTitle}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.score,
+                            color: Colors.amber,
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            'Score: ${quizResult.score}%',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.date_range,
+                            color: Colors.blue,
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            'Date: ${DateFormat('MMM d, h:mm a').format(quizResult.date)}',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.question_answer,
+                            color: Colors.green,
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            '${quizResult.countQuestion} questions',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('Delete Quiz Result'),
+                          content: Text('Are you sure you want to delete this quiz result?'),
+                          actions: [
+                            TextButton(
+                              child: Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                            TextButton(
+                              child: Text('Delete'),
+                              onPressed: () {
+                                deleteQuizResult(quizResult);
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+          }
+        } else {
+          return Center(
+            child: Text(
+              'No quiz results found.',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+
+
 /* class _ActivityTabState extends State<ActivityTab> {
   Future<List<QuizResult>> fetchQuizResults() async {
     final quizResultData = QuizResultStorage();
@@ -144,172 +328,172 @@ class ActivityTab extends StatefulWidget {
   }
 } */
 
-class _ActivityTabState extends State<ActivityTab> {
-  late Future<List<QuizResult>> _quizResultsFuture;
+// class _ActivityTabState extends State<ActivityTab> {
+//   late Future<List<QuizResult>> _quizResultsFuture;
 
-  @override
-  void initState() {
-    super.initState();
-    _quizResultsFuture = fetchQuizResults();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _quizResultsFuture = fetchQuizResults();
+//   }
 
-  Future<List<QuizResult>> fetchQuizResults() async {
-    final quizResultData = QuizResultStorage();
-    List<QuizResult> quizResults = await quizResultData.getQuizResults();
-    return quizResults.reversed.toList();
-  }
+//   Future<List<QuizResult>> fetchQuizResults() async {
+//     final quizResultData = QuizResultStorage();
+//     List<QuizResult> quizResults = await quizResultData.getQuizResults();
+//     return quizResults.reversed.toList();
+//   }
 
-  void deleteQuizResult(QuizResult quizResult) async {
-    print('Deleting quiz result with ID: ${quizResult.id}');
-    final quizResultData = QuizResultStorage();
-    await quizResultData.deleteQuizResult(quizResult);
+//   void deleteQuizResult(QuizResult quizResult) async {
+//     print('Deleting quiz result with ID: ${quizResult.id}');
+//     final quizResultData = QuizResultStorage();
+//     await quizResultData.deleteQuizResult(quizResult);
 
-    setState(() {
-      _quizResultsFuture = fetchQuizResults();
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<QuizResult>>(
-      future: fetchQuizResults(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(
-              color: Colors.purple,
-              strokeWidth: 6,
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text(
-              'Error: ${snapshot.error}',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          );
-        } else if (snapshot.hasData) {
-          final quizResults = snapshot.data!;
-          return ListView.builder(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            itemCount: quizResults.length,
-            itemBuilder: (context, index) {
-              final quizResult = quizResults[index];
-              return Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ListTile(
-                  title: Text(
-                    'activityname'.tr() + ' : ${quizResult.quizTitle}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepPurple,
-                    ),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.score,
-                            color: Colors.amber,
-                          ),
-                          SizedBox(width: 4),
-                          Text(
-                            'Score: ${quizResult.score}%',
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 4),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.date_range,
-                            color: Colors.blue,
-                          ),
-                          SizedBox(width: 4),
-                          Text(
-                            'Date: ${DateFormat('MMM d, h:mm a').format(quizResult.date)}',
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 4),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.question_answer,
-                            color: Colors.green,
-                          ),
-                          SizedBox(width: 4),
-                          Text(
-                            '${quizResult.countQuestion} questions',
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: Text('Delete Quiz Result'),
-                          content: Text('Are you sure you want to delete this quiz result?'),
-                          actions: [
-                            TextButton(
-                              child: Text('Cancel'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                            TextButton(
-                              child: Text('Delete'),
-                              onPressed: () {
-                                deleteQuizResult(quizResult);
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              );
-            },
-          );
-        } else {
-          return Center(
-            child: Text(
-              'No quiz results found.',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          );
-        }
-      },
-    );
-  }
-}
+//     setState(() {
+//       _quizResultsFuture = fetchQuizResults();
+//     });
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<List<QuizResult>>(
+//       future: fetchQuizResults(),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return Center(
+//             child: CircularProgressIndicator(
+//               color: Colors.purple,
+//               strokeWidth: 6,
+//             ),
+//           );
+//         } else if (snapshot.hasError) {
+//           return Center(
+//             child: Text(
+//               'Error: ${snapshot.error}',
+//               style: TextStyle(
+//                 color: Colors.red,
+//                 fontSize: 16,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//           );
+//         } else if (snapshot.hasData) {
+//           final quizResults = snapshot.data!;
+//           return ListView.builder(
+//             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+//             itemCount: quizResults.length,
+//             itemBuilder: (context, index) {
+//               final quizResult = quizResults[index];
+//               return Card(
+//                 elevation: 4,
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(10),
+//                 ),
+//                 child: ListTile(
+//                   title: Text(
+//                     'activityname'.tr() + ' : ${quizResult.quizTitle}',
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.deepPurple,
+//                     ),
+//                   ),
+//                   subtitle: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Row(
+//                         children: [
+//                           Icon(
+//                             Icons.score,
+//                             color: Colors.amber,
+//                           ),
+//                           SizedBox(width: 4),
+//                           Text(
+//                             'Score: ${quizResult.score}%',
+//                             style: TextStyle(
+//                               color: Colors.black,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                       SizedBox(height: 4),
+//                       Row(
+//                         children: [
+//                           Icon(
+//                             Icons.date_range,
+//                             color: Colors.blue,
+//                           ),
+//                           SizedBox(width: 4),
+//                           Text(
+//                             'Date: ${DateFormat('MMM d, h:mm a').format(quizResult.date)}',
+//                             style: TextStyle(
+//                               color: Colors.black,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                       SizedBox(height: 4),
+//                       Row(
+//                         children: [
+//                           Icon(
+//                             Icons.question_answer,
+//                             color: Colors.green,
+//                           ),
+//                           SizedBox(width: 4),
+//                           Text(
+//                             '${quizResult.countQuestion} questions',
+//                             style: TextStyle(
+//                               color: Colors.black,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                   trailing: IconButton(
+//                     icon: Icon(Icons.delete),
+//                     onPressed: () {
+//                       showDialog(
+//                         context: context,
+//                         builder: (context) => AlertDialog(
+//                           title: Text('Delete Quiz Result'),
+//                           content: Text('Are you sure you want to delete this quiz result?'),
+//                           actions: [
+//                             TextButton(
+//                               child: Text('Cancel'),
+//                               onPressed: () {
+//                                 Navigator.pop(context);
+//                               },
+//                             ),
+//                             TextButton(
+//                               child: Text('Delete'),
+//                               onPressed: () {
+//                                 deleteQuizResult(quizResult);
+//                                 Navigator.pop(context);
+//                               },
+//                             ),
+//                           ],
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//               );
+//             },
+//           );
+//         } else {
+//           return Center(
+//             child: Text(
+//               'No quiz results found.',
+//               style: TextStyle(
+//                 color: Colors.grey,
+//                 fontSize: 16,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//           );
+//         }
+//       },
+//     );
+//   }
+// }
 
 
 class QuizResultDetailPage extends StatelessWidget {
