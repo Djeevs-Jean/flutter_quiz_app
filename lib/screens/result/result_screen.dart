bc867b2fbdfb3cc4fe168e6b2438e6105ed00a05
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-07-04 05:12:47 -0400

    result screen page

--- lib/screens/result/result_screen.dart
+++ lib/screens/result/result_screen.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_quiz/model/question.dart';
 import 'package:flutter_quiz/main.dart';
 import 'package:flutter_quiz/service/service_sharepreferences.dart';
-import 'package:flutter_quiz/adapter/crud_hive.dart';
-
-import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
   final int score;
@@ -120,75 +117,3 @@ class ResultScreen extends StatelessWidget {
     );
   }
 }
-
-/* class ResultScreen extends StatelessWidget {
-  final int score;
-  final int totalQuestions;
-  final String titleQuiz;
-
-  const ResultScreen({Key? key, required this.score, required this.totalQuestions, required this.titleQuiz})  : super(key: key);
-
-  void saveScore(int count) async {
-    final scores = double.parse(getScorePercentage().toStringAsFixed(1));
-
-    final quizResult = QuizResult(
-      quizTitle: titleQuiz,
-      score: scores,
-      date: DateTime.now(),
-      countQuestion: count,
-    );
-
-    final quizResultData = QuizResultStorage();
-    await quizResultData.saveQuizResult(quizResult);
-
-  }
-  double getScorePercentage() {
-    double percentage = (score / totalQuestions) * 100;
-    return percentage;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    saveScore(totalQuestions);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Quiz Results'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Quiz Completed!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Your score: $score / $totalQuestions',
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Equivalent: ${getScorePercentage().toStringAsFixed(1)}%',
-              style: const TextStyle(fontSize: 20),
-            ),
-            
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Navigator.pop(context);
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MainScreen()),
-                  (route) => false,
-                );
-              },
-              child: const Text('Go to Home'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
- */
